b00d86ec365b9375b555f56461562cc3197624c5
Pavel <pavel_r_k9@mail.ru>
2025-04-10 12:23:23 +0300

    12 спринт финал разделение фильм сервиса на круды и лайки

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmCrudService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmCrudService.java
@@ -5,8 +5,12 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.Collection;
 
 public interface FilmCrudService {
+
     Film create(Film film);
+
     Film update(Film film);
+
     Collection<Film> findAll();
+
     Film findById(Long id);
 }
